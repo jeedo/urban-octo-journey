b1e846bdcf6519c284d444ca74cf9be950155e5f
Gede Suparsa <gede.suparsa@gmail.com>
2016-06-23 02:44:48 +1000

    added flask app

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ run apt-get -y update && apt-get install -y git
 run apt-get install -y python2.7
 run apt-get install -y python-pip
 run pip install gitpython
+run pip install Flask
--- gogit.py
+++ gogit.py
@@ -1,7 +1,23 @@
 from git import *
-#join = os.path.join
-print dir()
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+import subprocess
+import json
+
+app = Flask(__name__)
+
 repo = Repo('/root/host')
 
+format = '\'{%n  "commit": "%H",%n  "abbreviated_commit": "%h",%n  "tree": "%T",%n  "abbreviated_tree": "%t",%n  "parent": "%P",%n  "abbreviated_parent": "%p",%n  "refs": "%D",%n  "encoding": "%e",%n  "subject": "%s",%n  "sanitized_subject_line": "%f",%n  "body": "%b",%n  "commit_notes": "%N",%n  "verification_flag": "%G?",%n  "signer": "%GS",%n  "signer_key": "%GK",%n  "author": {%n    "name": "%aN",%n    "email": "%aE",%n    "date": "%aD"%n  },%n  "commiter": {%n    "name": "%cN",%n    "email": "%cE",%n    "date": "%cD"%n  }%n},\''
 
-print repo.commits()
+@app.route("/")
+def showlog():
+    output = subprocess.Popen(["git log --pretty=format:" + format], shell=True, stdout=subprocess.PIPE).communicate()[0]
+    #return output
+    output = '['+ output[:-1]+']'
+    print output
+    logdata = json.loads(output)
+    return render_template('showlog.html', logdata=logdata)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
